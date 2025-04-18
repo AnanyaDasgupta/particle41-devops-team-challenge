6356e0ea96cf0c5856f2cd937c0fdc2b3379bf0f
AnanyaDasgupta <ananyadasgupta280@gmail.com>
2025-04-18 13:56:11 +0000

    remove lock config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket         = "simpletime-state-bucket"
     key            = "simple-time-service.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-table"
   }
 }
